c201fa8b76f88010ede130a9f684c576dda021bb
Le Xuan Hoang Son <hoangson14112004@gmail.com>
2026-06-29 10:28:57 +0700

    Remove-and-Recreate-lh-wh

--- wh-vule-sonle-WWI-medallion.Warehouse/Sales/Tables/BuyingGroups.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [Sales].[BuyingGroups] (
-
-	[BuyingGroupID] int NULL, 
-	[BuyingGroupName] varchar(8000) NULL, 
-	[LastEditedBy] int NULL, 
-	[ValidFrom] datetime2(6) NULL, 
-	[ValidTo] datetime2(6) NULL
-);
--- wh-vule-sonle-WWI-medallion.Warehouse/Warehouse/Tables/StockGroups.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [Warehouse].[StockGroups] (
-
-	[StockGroupID] int NULL, 
-	[StockGroupName] varchar(8000) NULL, 
-	[LastEditedBy] int NULL, 
-	[ValidFrom] datetime2(6) NULL, 
-	[ValidTo] datetime2(6) NULL
-);
